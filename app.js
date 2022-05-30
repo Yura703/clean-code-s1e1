61014966712e4495e7deecbce2ef59710f7d74e0
Yura Lapitski <lyurik@tut.by>
2022-05-30 16:51:02 +0300

    fix: bringing class names in accordance with BEM. Refinement of js  for new classes to rule 2.1 in index.html, style.css and app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.className = "item";
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
@@ -33,18 +33,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task item__label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "item__input-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task item__input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="button edit";
 
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
